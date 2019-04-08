89afabe59547e56fab316263ba196c1c6eaac17a
Mauro Fioravanzi <mauro.fioravanzi@mondadori.it>
2019-04-08 12:39:04 +0200

    SEO HP

--- wp-content/themes/rcuoghi_public/home-catalogo.php
+++ wp-content/themes/rcuoghi_public/home-catalogo.php
@@ -3,15 +3,9 @@
 
 <div class="container-full homepage">
 
-	<div id="heading">
-		<h1>ROBERTO CUOGHI</h1>
-		<p>This is the official website of Roberto Cuoghi (born September 23, 1973, Modena, Italy)</p>
-	</div>
-
-	<a href="<?php echo get_site_url().'/artworks/'; ?>" title="ROBERTO CUOGHI / ARTWORKS" class="enter"></a>
-
 		<div class="onecol">
-				<?php the_content(); ?>
+			<h1><a href="<?php echo get_site_url().'/artworks/'; ?>" title="Roberto Cuoghi artworks"><img src="https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst.png" alt="This is the official website of Roberto Cuoghi (born September 23, 1973, Modena, Italy)" class="wp-image-1572" srcset="https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst.png 1000w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-508x124.png 508w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-973x238.png 973w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-25x6.png 25w" sizes="(max-width: 1000px) 100vw, 1000px"></a></h1>
+			<?php the_content(); ?>
 		</div>
 
 
