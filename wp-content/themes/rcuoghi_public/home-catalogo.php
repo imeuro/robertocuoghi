4d794cca3d9438ec59a2dbf18c4f5d89de22a087
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-09-28 13:45:35 +0200

    video intro - change on resize

--- wp-content/themes/rcuoghi_public/home-catalogo.php
+++ wp-content/themes/rcuoghi_public/home-catalogo.php
@@ -3,23 +3,18 @@
 
 <div class="container-full homepage">
 
-		<div class="onecol">
-			<h1><a href="<?php echo get_site_url().'/artworks/'; ?>" title="Roberto Cuoghi artworks"><img src="https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst.png" alt="This is the official website of Roberto Cuoghi (born September 23, 1973, Modena, Italy)" class="wp-image-1572" srcset="https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst.png 1000w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-508x124.png 508w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-973x238.png 973w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-25x6.png 25w" sizes="(max-width: 1000px) 100vw, 1000px"></a></h1>
-			<?php the_content(); ?>
-		</div>
-
+    <div class="onecol">
+        <h1><a href="<?php echo get_site_url().'/artworks/'; ?>" title="Roberto Cuoghi artworks"><img src="https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst.png" alt="This is the official website of Roberto Cuoghi (born September 23, 1973, Modena, Italy)" class="wp-image-1572" srcset="https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst.png 1000w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-508x124.png 508w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-973x238.png 973w, https://www.robertocuoghi.com/wp-content/uploads/2019/02/inst-25x6.png 25w" sizes="(max-width: 1000px) 100vw, 1000px"></a></h1>
+        <?php the_content(); ?>
+    </div>
 
 </div>
 
 <!-- ARTIFIZIO OMINI -->
 <div id="videocontainer" class="fullscreen-video"> 
-        <video id="videocontent" autoplay loop muted playsinline> 
-            <source src="https://www.robertocuoghi.com/wp-content/uploads/intro/Retrobalera_mob.mp4" type=video/mp4  media="all and (max-width: 480px)">
-            <source src="https://www.robertocuoghi.com/wp-content/uploads/intro/Retrobalera_1080.mp4" type=video/mp4>
-            <source src="https://www.robertocuoghi.com/wp-content/uploads/intro/Retrobalera_1080.webm" type=video/webm>
-        </video>
-    </div>
-    <style>
+    <video id="videocontent" autoplay loop muted playsinline></video>
+</div>
+<style>
     body {
         overflow: hidden;
         position: fixed;
@@ -47,32 +42,59 @@
         left: 50%;
         transform: translateX(-50%);
     }
-    </style>
-    <script>
-        var Vcont = document.getElementById('videocontainer');
-        var Vtag = document.getElementById('videocontent');
-        var Pcont = document.querySelector('.homepage .onecol');
+</style>
+<script>
+    var Vcont = document.getElementById('videocontainer');
+    var Vtag = document.getElementById('videocontent');
+    var Pcont = document.querySelector('.homepage .onecol');
+    
+    var Vurl = 'https://www.robertocuoghi.com/wp-content/uploads/intro/Retrobalera_1080';
 
-		Pcont.classList.add('hidden'); // ASAP!
+    Pcont.classList.add('hidden'); // ASAP!
 
-        Vcont.addEventListener("click", Vfadeout);
-        function Vfadeout() {
-            // console.log('Vcont clicked');
-            Vcont.className += ' hidden';
-            setTimeout(() => {
-                Vcont.remove();
-            }, 500);
-        };
+    var chooseVideoFormat = function() {
+        var sw = window.innerWidth;
+        if (sw < 640) {
+            Vurl = 'https://www.robertocuoghi.com/wp-content/uploads/intro/Retrobalera_mob';
+        }
+        var Vsource1 = document.createElement('source');
+        Vsource1.setAttribute('type','video/mp4');
+        Vsource1.setAttribute('src',Vurl + '.mp4');
+        var Vsource2 = document.createElement('source');
+        Vsource2.setAttribute('type','video/webm');
+        Vsource2.setAttribute('src',Vurl + '.webm');
+        Vtag.innerHTML = "";
+        Vtag.append(Vsource1);
+        Vtag.append(Vsource2);
+        Vtag.load();
 
-        Vtag.addEventListener("canplay", BGfadein);
-        function BGfadein() {
-            console.log('BGfadein running');
-			Vtag.play();
-            setTimeout(() => {
-                Pcont.classList.remove('hidden');
-            }, 500);
-        };
-    </script>
+        //console.log('video chosen: '+Vurl);
+    }
+
+    chooseVideoFormat();
+
+    window.onresize = function(event) {
+        setTimeout(chooseVideoFormat(),500)
+    };
+
+    Vcont.addEventListener("click", Vfadeout);
+    function Vfadeout() {
+        // console.log('Vcont clicked');
+        Vcont.className += ' hidden';
+        setTimeout(() => {
+            Vcont.remove();
+        }, 500);
+    };
+
+    Vtag.addEventListener("canplay", BGfadein);
+    function BGfadein() {
+        console.log('BGfadein running');
+        Vtag.play();
+        setTimeout(() => {
+            Pcont.classList.remove('hidden');
+        }, 500);
+    };
+</script>
 <!-- END ARTIFIZIO OMINI -->
 
 
