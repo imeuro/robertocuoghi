0a48d41902ccc3ccad12a1791807f172bf6e9431
imeuro <mauro.fioravanzi@gmail.com>
2024-11-20 11:27:34 +0100

    first impl

--- wp-content/themes/rcuoghi_public/archive-artworks.php
+++ wp-content/themes/rcuoghi_public/archive-artworks.php
@@ -32,18 +32,52 @@ get_header();
 				<article id="thumb-<?php the_ID(); ?>" <?php post_class('rc-square-thumb'); ?>>
 					<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 						<?php
+
+						// PIC SIZE IS BASED ON REAL WORLD DIMENSIONS
+						
+						$dims = get_field('art_dimensions');
+						//$string = "200 x 554 x 5 cm / 78.74 x 218.11 x 21.65 in";
+						$pattern = "/(\d+(?:\.\d+)?) (x|×) (\d+(?:\.\d+)?)/";
+						preg_match($pattern, $dims, $matches);
+						
+						/*
+						echo '<pre>';
+						print_r($dims);
+						echo '<br>';
+						print_r($matches);
+						echo '<br>';
+						print_r($matches[1]);
+						echo '<br>';
+						print_r($matches[32]);
+						echo '<br>';
+						print_r(($matches[1] * $matches[3] ));
+						echo '<br>';
+						print_r(($matches[1] * $matches[3] )/5000);
+						echo '</pre>';
+						*/
+
 						$custom_thumb_id= get_field('custom_thumb',get_the_ID());
 						if ($custom_thumb_id && $custom_thumb_id != '') :
 							$ThumbImgData = wp_get_attachment_image_src( $custom_thumb_id, $picsz );
 						else :
 							$ThumbImgData = wp_get_attachment_image_src( get_post_thumbnail_id(), $picsz );
 						endif;
-						echo '<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="'.$ThumbImgData[0].'" width="'.$ThumbImgData[1].'" height="'.$ThumbImgData[2].'" alt ="Roberto Cuoghi - '.the_title_attribute( array( 'echo' => false, ) ).'" class="rc-lazyload" />';
+
+						if (isset($matches[3]) && $matches[3] != null) :
+							$multiplier = ($matches[3] / $ThumbImgData[1])*400;
+						else :
+							$multiplier = 100;
+						endif;
+
+						echo '<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="'.$ThumbImgData[0].'" width="'.$multiplier.'%" height="'.$ThumbImgData[2]*$multiplier.'" alt ="Roberto Cuoghi - '.the_title_attribute( array( 'echo' => false, ) ).'" class="rc-lazyload" />';
+
 						?>
 					</a>
 				</article>
 
-			<?php }
+			<?php 
+			// echo '$multiplier: '.$multiplier;
+			}
 			the_posts_navigation(); ?>
 
 		</div>
@@ -58,12 +92,6 @@ get_header();
 
 		<aside class="artworks-navi <?php echo 'render-'.$pcls; ?>">
 			<ul id="cat-filter"><?php get_template_part( 'template-parts/artworks', 'nav' ); ?></ul>
-
-			<!-- <div id="wip">
-				<p><strong>Please note:</strong> as this is a work in progress, some artworks may be missing. The full collection will be available before the official release.</p>
-				<a class="close">dismiss</a>
-			</div> -->
-
 		</aside>
 
 
