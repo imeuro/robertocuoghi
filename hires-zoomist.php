c773778abd753a918ee5cbbba7b076947ced3011
imeuro <mauro.fioravanzi@gmail.com>
2024-11-25 14:00:22 +0100

    test zoom

--- hires-zoomist.php
+++ hires-zoomist.php
@@ -79,7 +79,7 @@
     import Zoomist from 'https://cdn.jsdelivr.net/npm/zoomist@2/zoomist.js'
     const zoomist = new Zoomist('.zoomist-container', {
         // Optional parameters
-        maxScale: 4,
+        maxScale: 8,
         bounds: true,
         // if you need slider
         slider: true,
